7ddcc0052a8677c85eeb7d982946b9aa60502ae8
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-10-25 15:20:13 -0400

    Update __init__.py

--- footballTimeCalculator/__init__.py
+++ footballTimeCalculator/__init__.py
@@ -1,11 +1,5 @@
-def convertFootballToReal(minutes):
-    return minutes * 2.5
-
-def _Interactive():
-    minutes = float(input("Please enter the current number of Football Minutes: "))
-    converted = convertFootballToReal(minutes)
-    print("Converted time: ", converted)
-    print("This is equal to %s real hours." % converted / 60)
-
-if __name__ == "__main__":
-    _Interactive()
+def convertFootballToReal(original):
+    """
+    parameter original: Original number
+    """
+    return original * 2.5
