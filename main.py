c4cdbd13c690c47dad3b12a4bbfe2df66ddc918f
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-10-25 15:28:46 -0400

    fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from footballTimeCalculator import convertFootballToReal
 
 minutes = float(input("Please enter the current number of Football Minutes: "))
-converted = convertFootballToReal(minutes)
+converted = float(convertFootballToReal(minutes))
 print("Converted time: ", converted)
 print("This is equal to %s real hours." % converted / 60)
