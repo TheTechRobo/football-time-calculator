e2afa8c8fcf101c399fb949b52c2ba66a72c2118
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-10-25 15:44:28 -0400

    Update __init__.py

--- footballTimeCalculator/__init__.py
+++ footballTimeCalculator/__init__.py
@@ -1,5 +1,6 @@
 def convertFootballTimeToReal(original):
     """
     parameter original: Original number
+    Algorithm comes from the fact that a football game is supposed to last 60 minutes, but actually lasts two and a half hours. So I multiply the rogiinal number by 2.5 and it works! :D (Testing it right now)
     """
     return original * 2.5
